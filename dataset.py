bcf0b8828bbc63f145cc062fbc0171234d4f3f7e
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2022-02-07 19:13:01 +0100

    added collator TO CHECK

--- dataset.py
+++ dataset.py
@@ -1,29 +1,30 @@
 import os
-from nltk.util import elementtree_indent
-import pickle5 as pickle
 
+import nltk
 import numpy as np
 import pandas as pd
+import pickle5 as pickle
 import torch
 from joblib.numpy_pickle_utils import xrange
-from nltk import WordNetLemmatizer
-from transformers import BertModel, BertTokenizer
-
-import params
-import nltk
-from nltk.corpus import stopwords
+from keras_preprocessing import sequence
 from nltk.corpus import sentiwordnet as swn
+from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
 from nltk.tokenize import word_tokenize
-from torch.utils.data import Dataset
 from sklearn.feature_extraction.text import TfidfVectorizer
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset
+from transformers import BertModel, BertTokenizer
+import torch
+import params
 
 
 def splitter(s, n):
     if not isinstance(s, list):
         s = s.split()
         for i in xrange(0, len(s), n):
-            yield ' '.join(s[i:i+n])
+            yield ' '.join(s[i:i + n])
     else:
         for i in range(0, len(s), n):
             yield s[i:i + n]
@@ -43,29 +44,34 @@ def penn_to_wn(tag):
 
 def get_sentiment(word: str) -> float:
     tag = nltk.pos_tag([word])[0][1]
-    #print(tag)
+    # print(tag)
     ss = wn.synsets(word)
     if ss:
         score = swn.senti_synset(ss.__getitem__(0).name())
-        word_score = (score.pos_score() + score.neg_score())/2
+        word_score = (score.pos_score() + score.neg_score()) / 2
         return word_score
     else:
         return 1.
 
 
+idx_to_remove = [226, 371, 412, 1884, 1887, 3613, 4906, 5010,
+                 5056, 5118, 5738, 5973, 6576, 8068, 8086, 8500,
+                 8636, 8713, 9120, 9151, 9182, 9303, 9355, 9571, 9790,
+                 9824, 10026, 10039, 10043, 10184]
+
+
 class AmazonDataset(Dataset):
     def __init__(self):
+        self.maximum_embedding_len = None
         self.tokenizer = None
         self.embedded_words_dict = None
         self.ranking = None
         self.tfidf = None
         self.labels = None
         self.data = None
-        self.embedded_data = None
         self.path = params.path_ds
         assert os.path.exists(self.path), "Please insert a valid dataset path"
-        self.loaded = os.path.exists(os.path.join(self.path, 'amazonDataset.pkl'))
-        #print(self.loaded)
+        self.loaded = os.path.exists(os.path.join(self.path, params.pickled_name))
 
     def clean_strings(self):
         to_remove = []
@@ -113,79 +119,97 @@ class AmazonDataset(Dataset):
         model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
         model.eval()
         embedded_data = {}
+        len_max = 0
         for i, sent in self.data.iteritems():
             tokenized_text = self.tokenizer.tokenize(sent)
-            input_id = self.tokenizer.convert_tokens_to_ids(tokenized_text)
-            attention_mask = [1] * len(tokenized_text)
-            input_ids = list(splitter(input_id, 510))
-            attention_masks = list(splitter(attention_mask, 510))
-            tokenized_texts = list(splitter(tokenized_text, 510))
-            for id_chunk, mask_chunk, test_chunk in zip(input_ids, attention_masks, tokenized_texts):
-                id_chunk = torch.cat([torch.tensor([101]), torch.tensor(id_chunk), torch.tensor([102])])
-                mask_chunk = torch.cat([torch.tensor([1]), torch.tensor(mask_chunk), torch.tensor([1])])
-                test_chunk = ["[CLS]"] + test_chunk + ["[SEP]"]
-                with torch.no_grad():
-                    assert len(id_chunk) == len(mask_chunk), \
-                        f"len(token_tensor) expected to be {len(id_chunk)}, but got {len(mask_chunk)}"
-                    id_chunk = torch.unsqueeze(id_chunk, 0)
-                    mask_chunk = torch.unsqueeze(mask_chunk, 0)
-                    outputs = model(id_chunk, mask_chunk)
-                    hidden_states = outputs[2]
-                token_embeddings = torch.stack(hidden_states, dim=0)
-                token_embeddings = torch.squeeze(token_embeddings, dim=1)
-                token_embeddings = token_embeddings.permute(1, 0, 2)
-                for j, token in enumerate(token_embeddings):
-                    cat_vec = torch.cat((token[-1], token[-2], token[-3], token[-4]), dim=0)
-                    embedded_data[test_chunk[j]] = cat_vec
+            if len(tokenized_text) <= params.MAX_SENT_LEN:
+                if len(tokenized_text) > len_max:
+                    len_max = len(tokenized_text)
+                input_id = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+                attention_mask = [1] * len(tokenized_text)
+                input_ids = list(splitter(input_id, 510))
+                attention_masks = list(splitter(attention_mask, 510))
+                tokenized_texts = list(splitter(tokenized_text, 510))
+                for id_chunk, mask_chunk, test_chunk in zip(input_ids, attention_masks, tokenized_texts):
+                    id_chunk = torch.cat([torch.tensor([101]), torch.tensor(id_chunk), torch.tensor([102])])
+                    mask_chunk = torch.cat([torch.tensor([1]), torch.tensor(mask_chunk), torch.tensor([1])])
+                    test_chunk = ["[CLS]"] + test_chunk + ["[SEP]"]
+                    with torch.no_grad():
+                        assert len(id_chunk) == len(mask_chunk), \
+                            f"len(token_tensor) expected to be {len(id_chunk)}, but got {len(mask_chunk)}"
+                        id_chunk = torch.unsqueeze(id_chunk, 0)
+                        mask_chunk = torch.unsqueeze(mask_chunk, 0)
+                        outputs = model(id_chunk, mask_chunk)
+                        hidden_states = outputs[2]
+                    token_embeddings = torch.stack(hidden_states, dim=0)
+                    token_embeddings = torch.squeeze(token_embeddings, dim=1)
+                    token_embeddings = token_embeddings.permute(1, 0, 2)
+                    for j, token in enumerate(token_embeddings):
+                        cat_vec = torch.cat((token[-1], token[-2], token[-3], token[-4]), dim=0)
+                        embedded_data[test_chunk[j]] = cat_vec
         self.embedded_words_dict = embedded_data
+        self.maximum_embedding_len = len_max
 
     def load_dataset(self):
         if not self.loaded:
-            #ds = pd.read_json (r'reviews.json',  lines=True)
-            ds = pd.read_csv(self.path+"reviews.csv", sep=';')
-            self.data = ds["reviewText"].copy(deep=True)
-            self.labels = ds["overall"].copy(deep=True)
+            amazon_ds = pd.read_csv(f"{self.path}/dataset.csv")
+            self.data = amazon_ds["reviewText"].copy(deep=True)
+            self.labels = amazon_ds["overall"].copy(deep=True)
             tmp1 = self.clean_strings()
             tmp2 = self.sw_removal()
             to_remove = np.append(tmp1, tmp2)
+            print(f"Dropped elements = {to_remove}")
             self.data.drop(to_remove, inplace=True)
             self.labels.drop(to_remove, inplace=True)
             self.bert_embedding()
-            with open(f'{self.path}amazonDataset.pkl', 'wb') as outp:
+            with open(f'{self.path}/{params.pickled_name}', 'wb') as outp:
                 pickle.dump(self, outp, pickle.HIGHEST_PROTOCOL)
         else:
-          
-            with open(f'{self.path}amazonDataset.pkl', 'rb') as inp:
-                
-                dataset = pickle.load(inp)
-                self.tokenizer = dataset.tokenizer
-                self.embedded_words_dict = dataset.embedded_words_dict
-                self.ranking = dataset.ranking
-                self.tfidf = dataset.tfidf
-                self.labels = dataset.labels
-                self.data = dataset.data
-                self.path = dataset.path
+            with open(f'{self.path}/{params.pickled_name}', 'rb') as inp:
+                amazon_ds = pickle.load(inp)
+                self.tokenizer = amazon_ds.tokenizer
+                self.embedded_words_dict = amazon_ds.embedded_words_dict
+                self.ranking = amazon_ds.ranking
+                self.tfidf = amazon_ds.tfidf
+                self.labels = amazon_ds.labels
+                self.data = amazon_ds.data
+                self.path = amazon_ds.path
+
+    def filter(self):
+        self.data.drop(idx_to_remove, inplace=True)
+        self.labels.drop(idx_to_remove, inplace=True)
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx):
         sent = self.data[idx]
+        lab = self.labels[idx]
         embedding = []
         tokenized_text = self.tokenizer.tokenize(sent)
         for token in tokenized_text:
             sent = get_sentiment(token)
-            embedding.append(self.embedded_words_dict[token]*sent)
-        return embedding
-
-    def format_data(self):
-        embedded = []
-        for idx in range(len(self.labels)):
-            embedded.append(self.__getitem__(idx))
-        self.embedded_data = embedded
-        return embedded
+            tmp = torch.Tensor(self.embedded_words_dict[token] * sent)
+            embedding.append(tmp)
+        return embedding, lab
 
 
-## Usage:
-# p = AmazonDataset()
-# p.load_dataset()
+## to check
+class Collator(object):
+    def __init__(self, test=False, percentile=100):
+        self.test = test
+        self.percentile = percentile
+
+    def __call__(self, batch):
+        global MAX_LEN
+        if self.test:
+            texts, lens = zip(*batch)
+        else:
+            texts, lens, target = zip(*batch)
+        lens = np.array(lens)
+        max_len = min(int(np.percentile(lens, self.percentile)), MAX_LEN)
+        texts = torch.tensor(sequence.pad_sequences(texts, maxlen=max_len), dtype=torch.long)
+        if self.test:
+            return texts
+
+        return texts, torch.tensor(target, dtype=torch.float32)
