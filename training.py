cd7caa25dd50f845c244a57c111a3233766ffe77
luciainnocenti <lucia.innoenti@outlook.it>
2022-02-15 09:10:32 +0100

    MSE with OHE

--- training.py
+++ training.py
@@ -1,6 +1,5 @@
 import numpy as np
 import torch
-import gc
 import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
@@ -14,6 +13,7 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.utils import class_weight
+from torch.optim.lr_scheduler import ExponentialLR
 import warnings
 warnings.filterwarnings("ignore")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -25,42 +25,38 @@ def train_val_dataset(d, val_split=0.25):
 
 
 print(f"Preparing...")
-ds = dataset.AmazonDataset()  # fare prova con anche dataset non caricato
+ds = dataset.AmazonDataset()
 ds.load_dataset()
 ds.filter()
+# ds.undersampling()
 weights = class_weight.compute_class_weight('balanced', classes=np.unique(ds.labels), y=ds.labels)
 weights = torch.Tensor(weights)
 train_ds, test_ds = train_val_dataset(ds)
 
 # Define parameters
 batch_size = params.BATCH_SIZE
-hidden_dim = 256
+hidden_dim = 128
 embedding_size = params.NUM_FEATURES
 dropout_rate = params.DROPOUT_RATE
-lr = params.LR
+lr = 0.001 # params.LR
 epochs = params.NUM_EPOCHS
 best_loss = float('inf')
-
 
 train_loader = DataLoader(train_ds, batch_size=params.BATCH_SIZE, collate_fn=dataset.collate_batch,
                           shuffle=True, num_workers=0, drop_last=True)
 # Build the model
-lstm_model = net.SentimentAnalysis(batch_size,
-                                   hidden_dim,
-                                   embedding_size,
-                                   dropout_rate)  # modificato LSTM con SentimentAnalysis (nome rete)
-
+lstm_model = net.SentimentAnalysis(batch_size, hidden_dim, embedding_size, dropout_rate)
 lstm_model.to(device)
+
 # optimization algorithm
 optimizer = torch.optim.Adam(lstm_model.parameters(), lr=lr)
-to_train = True
-ce = nn.CrossEntropyLoss(weight=weights)
+scheduler = ExponentialLR(optimizer, gamma=0.9)
+ce = nn.CrossEntropyLoss()  # weight=weights)
 bce = nn.BCEWithLogitsLoss(pos_weight=weights)
 mse = nn.MSELoss()
 softmax = torch.nn.Softmax(dim=1)
 
 lstm_model = lstm_model.train()
-
 
 # train and validate
 print("Start running")
@@ -71,22 +67,24 @@ with torch.enable_grad():
         print(f"Epoch: {epoch}")
         for idxs, (batch, labels) in enumerate(train_loader):
             batch = batch.to(device)
-            labels = labels.to(device)
             predictions = lstm_model(batch).to('cpu')
             labels = torch.Tensor([x - 1 for x in labels.data.numpy()])  # mapping classes 1-5 in 0-4
             long_labels = labels.type(torch.LongTensor)
-            loss1 = 0.5 * ce(predictions, long_labels)
-            # loss1 = 0.5 * bce(predictions, nn.functional.one_hot(long_labels).float())
+            loss1 = ce(predictions, long_labels) * 0.5
+            # loss1 = bce(predictions, nn.functional.one_hot(long_labels).float())  # * 0.5
             float_preds = torch.argmax(softmax(predictions), 1)
             float_preds = float_preds.type(torch.FloatTensor)
             float_preds.requires_grad = True
-            loss2 = 0.5 * torch.sqrt(mse(float_preds, labels))
 
-            loss1.backward()
-            loss2.backward()
+            ohe_labels = nn.functional.one_hot(long_labels).float()
+            sm_preds = softmax(predictions)
+            loss2 = torch.sqrt(mse(sm_preds, ohe_labels)) * 0.5
+            # loss2 = torch.sqrt(mse(float_preds, labels)) * 0.5
+            loss = loss1 + loss2
+            loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            loss = loss1.item() + loss2.item()
+            loss = loss.item()
             del loss1
             del loss2
             accuracy = accuracy_score(float_preds.data, long_labels)
@@ -102,7 +100,7 @@ with torch.enable_grad():
             epoch_recall = epoch_recall + recall
             if not idxs % 10:
                 print(f"Loss: {loss}, Accuracy: {accuracy}, F1: {f1}")
-
+        scheduler.step()
         train_loss, train_acc = epoch_loss / len(train_loader), epoch_acc / len(train_loader)
         train_f1, train_precision = epoch_f1 / len(train_loader), epoch_precision / len(train_loader)
         train_recall = epoch_recall / len(train_loader)
@@ -110,8 +108,8 @@ with torch.enable_grad():
         if train_loss < best_loss:
             best_valid_loss = train_loss
             torch.save(lstm_model.state_dict(), 'saved_weights_BiLSTM.pt')
-        print(f'\tEpoch; {epoch} | Train Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
-        print(f"F1 = {train_f1}, precision = {train_precision}, recall = {train_recall}")
+        print(f'Epoch; {epoch} | Train Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
+        print(f"\tF1 = {train_f1}, precision = {train_precision}, recall = {train_recall}")
 
 # load weights and make predictions
 lstm_model.load_state_dict(torch.load("saved_weights_BiLSTM.pt"))
