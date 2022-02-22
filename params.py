b0097677e85365d66851d9c85c1a01c717348a1e
andrerubeis <andrea.rubeis@outlook.it>
2022-02-22 22:58:34 +0100

    fixed few typo

--- params.py
+++ params.py
@@ -12,14 +12,14 @@ nltk.download('sentiwordnet')
 nltk.download('averaged_perceptron_tagger')
 
 NUM_CLASSES = 5
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 NUM_EPOCHS = 8
 LR = 1e-3
 DROPOUT_RATE = 0.4
 SEED = 42
 MAX_SENT_LEN = 500
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-EMBEDDING_TYPE = 'concat' # 'avg'
+EMBEDDING_TYPE = 'avg' # 'concat'
 LOSS1 = 'ce' # 'bce'
 LOSS2 = 'mse' # '1HEMSE'
 NUM_FEATURES = 768 if EMBEDDING_TYPE == 'avg' else 3072
